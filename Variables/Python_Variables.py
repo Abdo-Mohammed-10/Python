b6445e3339f731afe1a450e5af2bda4bb191dc24
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-06-19 20:08:41 +0300

    Look Readme

---
+++ Variables/Python_Variables.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+# # In[1]:
+#types of variables
+
+
+
+hello="Abdulrahman Mohammed"
+print (hello)
+print(type(hello))#---->>string
+
+
+
+
+
+name=input(" entre your name ")
+print ("hello", name)
+
+
+
+
+
+x=13
+print(type(x))
+#or 
+type(x)
+
+
+
+
+x=10.22
+
+print(type(x))
+type(x)
+
+
+
+
+x=55+11J
+print(type(x))
+type(x)
+
+
+
+
+x="abdo"
+print(type(x))
+type(x)
+
+
+
+
+x=(2,3,6,4) #tuble using cotes()
+print(type(x))
+
+
+
+
+x=[13,5,365] #list using []
+print(type(x))
+
+
+
+
+x={45,232,98} #set using {}
+print(type(x))
+
+
+
+
+x=open('Python_Variables.ipynb','r') #you shoulde create that file : "Python_Variables.ipynb" to continue without error
+print(type(x))
+
+
+
+
+x=y=z=2
+print(x,y,z)
+
+
+
+
+(x,y,z)=1,6,8
+print(x)
+print(y)
+print(z)
+
+
+
+
+# rule for NAMING VARIABLES 
+
+
+
+
+## @abd=14
+#withouut charachter in frist
+
+
+
+
+## 2sd="1dxx"
+#NO NUMBER first of naming variable
+
+
+
+
+# a646&=16
+#not true
+
+
+
+
+## axcd-33=15
+#that not true
+
+
+
+
+ABB5="CCK"
+# THAT IS TRUE
+
+
+
+
+_AVV=33
+#THAT IS TRUE
+
+
+
+
+_ada_=16
+#that is true
+
+
+
+
+print(_AVV,_ada_)
+
+
+
+
+# don't use special meaning word when you naming variable like : class =5
+#                                                               break="abdo"
+#                                                               try=13
+
+
+#GOOD LUCK
+                                 #created by Abdulrahman Mohammed 
+
