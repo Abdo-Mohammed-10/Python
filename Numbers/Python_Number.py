45dccf1a5151402aa7b3eeaf5d58705c587c8753
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-06-21 01:18:15 +0300

    Number in Python

---
+++ Numbers/Python_Number.py
@@ -0,0 +1,159 @@
+
+
+
+#PYTHON NUMBER
+
+
+
+
+n=5
+type(n)
+
+
+
+
+n=26+5j 
+#     note
+#        5j --->> complex number 
+#     n-->>  real part = 26 
+#     n-->>  imag part = 5
+print(type(n))
+n.real
+
+
+
+
+n.imag
+
+
+
+
+print(6/5) 
+print(6//5)# print integer number of 6/5
+print("---------------")
+print(10/7)
+print(10//7)
+print("---------------")
+print(11/6)
+print(11//6)
+print("---------------")
+print(5**6)# -----> thats mean 5^6 
+
+
+
+
+x=1
+y=10.22
+z=15+16j
+print(type(x)) 
+print(type(y))
+print(type(z))
+print("---------------")
+print(z.real) 
+print(z.imag) 
+print("---------------")
+print(z.real+22) 
+print(z.imag+12) # --->> print part imag + 12 --> 12+16
+print("---------------")
+
+
+
+
+
+
+# conversion
+
+x="165"
+print(x)
+type (x)
+
+
+
+x=int(x)
+print(x)
+type(x)
+
+
+
+
+x=float(x)
+print(x)
+type(x)
+
+
+
+
+x=complex(x)
+print(x)
+type(x)
+
+
+
+
+complex(5,3)
+
+
+
+
+x=complex(6,10)
+print(x)
+
+
+
+
+x=int(10.32)
+print(x)
+
+
+
+
+x=float(10)
+print(x)
+
+
+
+
+# functions
+
+
+
+
+x=-15
+print(abs(x))#---> abs value of x
+
+
+
+
+#math مكتبة ال
+
+
+
+
+import math            # important to import it
+
+
+
+
+x=66
+print(math.exp(x)) # that is mean e^x 
+print(math.e)# --> value of e
+print(math.pi)# value of pi
+print(math.sqrt(x)) # square root of x 
+print(math.sqrt(66))# square root of x 
+
+
+
+
+max(2,36,5,99)
+
+
+
+
+min(2,1,16,0,-55)
+
+
+
+
+# GOOD LUCK 
+                                # created by Abdulrahman Mohammed 
+
