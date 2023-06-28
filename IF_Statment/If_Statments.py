810a09d228a5fa0bdbb1d54cf7274f99b77cf5b3
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-06-29 01:28:52 +0300

    Three types of IF_Statments

---
+++ IF_Statment/If_Statments.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IF _ ELSE_STATEMENTS
+
+
+
+i=20
+if i%2==0:
+      print("  even number")
+else:
+    print("not even")
+
+
+# #                                   NESTED _IF
+
+
+
+c=22
+if c<25:
+    if c%2==0:
+        print("even and less 25")
+    else: 
+        print("odd and less 25")
+else :
+        print("greater than 25")
+
+
+# #                                 IF_ELIF_ELSE
+
+
+
+var="Z"
+if var =="c":
+    print("that is true is equal to c")
+elif var=="a":
+        print("equal a")
+elif var=="b":
+            print("equal to b")
+else :
+                print("is constant")
+    
+
+
+# #    GOOOD LUCK
+#  Abdulrahman Mohammed
