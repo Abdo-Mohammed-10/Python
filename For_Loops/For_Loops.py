ab7471e42888f357bb87ca47c382a9e17f8993d1
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-04 01:38:27 +0300

    For_Loops.py

--- For_Loops/For_Loops.py
+++ For_Loops/For_Loops.py
@@ -3,40 +3,39 @@
 
 # #            FOR LOOPS
 
-# In[4]:
+
 
 
 x=[12,3,11]
    
 
 
-# In[5]:
+
 
 
 print(x[0])
 
 
-# In[6]:
+
 
 
 print(x[0],x[1])
 
 
-# In[7]:
 
 
 for i in range(1,12):
     print(i)
 
 
-# In[8]:
+
 
 
 for i in range(1,20,2) :  
     print(i)
 
 
-# In[17]:
+
 
 
 sum=0
@@ -44,8 +43,6 @@ for i in range (1,5):
     sum=sum+i
     print(sum)
 
-
-# In[19]:
 
 
 sum=0
@@ -55,7 +52,6 @@ for i in range (1,5):
     print(sum)
 
 
-# In[12]:
 
 
 n=int(input("enter number"))
@@ -65,7 +61,7 @@ for i in range(1,n+1):
     print()
 
 
-# In[10]:
+
 
 
 r=int(input("entre r "))
