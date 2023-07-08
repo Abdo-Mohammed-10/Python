1b534ba72b7d6708b0c4e5099af4bf9e815715c9
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-08 09:17:14 +0300

    Array_in_python

---
+++ Array/ARRAY.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+from array import*
+arr =array("d",[2,5,9,6])
+print(arr)
+
+
+
+
+print(arr.buffer_info())
+
+
+
+
+arr[2]
+
+
+
+
+for i in arr:
+    print(i)
+    
+
+
+
+
+for pnt in range (4) :
+    print( arr[pnt])
+
+
+
+
+arr.reverse()
+
+
+
+
+print(arr)
+
+
+
+
+arr.append(50)
+print(arr)
+
+
+
+
+arr.remove(9)
+print(arr)
+
+
+# # GOOD LUCK
+# Abdulrahman Mohammed 
