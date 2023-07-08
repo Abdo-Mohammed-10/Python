db1449be82a3655cc4454932e6349b10f92f60c5
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-09 00:23:15 +0300

    Simple_Functions

---
+++ Functions/Functions.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # FUNCTION
+
+
+
+def welcome():
+    print("cool")
+welcome()
+
+
+
+
+def add(a,b):
+    total=a+b
+    print(total)
+    
+
+
+
+
+add(15,66)
+
+
+
+
+def add(a=0,b=0):
+    total=a+b
+    print(total)
+add(10)
+
+
+
+
+def add (*a):
+    total=0                    #note vr
+    for i in a :
+        total =total+i
+        print ("the sum", total)
+
+
+
+
+add(166)
+
+
+
+
+add(1,2,3,4)
+
+
+# # Good luck
+# Abdulrahman Mohammed
